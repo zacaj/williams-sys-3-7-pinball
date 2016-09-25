582ee177244154bab6ad7eb54157556e95c59d2b
zacaj <github@zacaj.com>
2016-09-25 17:42:22 -0400

    switch input

--- test.asm
+++ test.asm
@@ -6,17 +6,23 @@ lampRow:		.equ $2400
 lampRowC:		.equ $2401
 lampStrobe:		.equ $2402
 lampStrobeC:	.equ $2403
+switchStrobe:	.equ $3002
+switchStrobeC:	.equ $3003
+switchRow:		.equ $3000
+switchRowC:		.equ $3001
 
 RAM:			.equ $0000
 cRAM:			.equ $0100
-strobe:			.equ RAM + $00
+displayColumn:			.equ RAM + $00
 counter:		.equ RAM + $01
 temp:			.equ RAM + $02
 counter2:		.equ RAM + $03
-lampColumn:		.equ RAM + $07
+strobe:		.equ RAM + $07
 lampRow1:		.equ RAM + $08
 
 displayBcd1:	.equ RAM + $10
+
+switchRow1:		.equ RAM + $1F
 
 main:		.org $7800
 	
@@ -49,6 +55,20 @@ piaSetup:
 	ldaA 	#00000100b 	;select data (3rb bit = 1)
 	staA 	lampStrobeC
 	
+	ldaA	#00000000b	;select direction (3rd bit = 0)
+	staA 	switchStrobeC
+	ldaA 	#11111111b	;set to output
+	staA 	switchStrobe
+	ldaA 	#00000100b 	;select data (3rb bit = 1)
+	staA 	switchStrobeC
+	
+	ldaA	#00000000b	;select direction (3rd bit = 0)
+	staA 	switchRowC
+	ldaA 	#00000000b	;set to input
+	staA 	switchRow
+	ldaA 	#00000100b 	;select data (3rb bit = 1)
+	staA 	switchRowC
+	
 	ldaA	#00
 	staB	displayBcd1
 	
@@ -56,7 +76,7 @@ piaSetup:
 	staA 	displayStrobe
 
 	ldaA	#00
-	staA	lampColumn
+	staA	strobe
 	
 	ldaA	#$F0
 	staA	lampRow1
@@ -86,32 +106,43 @@ on:
 	staA	displayBcd1	
 
 counterHandled:
-
+; move switch column
+	ldaA	strobe
+	staA	switchStrobe
+	
+; update display 
 	ldaA	#11110000b	
-	oraA	strobe	
+	oraA	displayColumn	
 	ldaB 	#$FF
 	staB	displayBcd
 	staA	displayStrobe
 	ldaB	displayBcd1
 	staB	displayBcd
-	inc 	strobe
+	inc 	displayColumn
 	
+; read switches
+	ldaA	switchRow
+	comA
+	staA	lampRow1 ; for debugging
+	staA 	switchRow1
+	
+; update lamps
 	ldaA	#$FF
 	staA	lampRow
-	ldaA	lampColumn
+	ldaA	strobe
 	staA	lampStrobe
 	ldaA	lampRow1
 	staA	lampRow
 	ldaA	#00
-	asl		lampColumn
-	cmpA	lampColumn
-	bne		lsNotZero
-	
-	ldaA	#00000001b
-	staA	lampColumn
-lsNotZero:
 
-	bra		loop
+	
+	asl		strobe
+	cmpA	strobe ; strobe done?  reset
+	bne		sNotZero
+		ldaA	#00000001b
+		staA	strobe
+sNotZero:
+		bra		loop
 	
 end:
 	bra		end
