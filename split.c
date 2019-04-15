8cd9226da192e9ce331b8a9747cad31bf68226b7
zacaj <github@zacaj.com>
2019-04-15 19:28:41 -0400

    parameterize split

--- split.c
+++ split.c
@@ -1,49 +1,80 @@
 #include <stdio.h>
 #include <errno.h>
-void write(FILE *in, FILE *out) {
-	char data[2048];
-	fread(data, 2048,1, in);
-	fwrite(data, 2048,1, out);
+void write(FILE *in, FILE *out, int amount) {
+	char data[amount];
+	fread(data, amount,1, in);
+	fwrite(data, amount,1, out);
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+	if(argc < 2) {
+		fprintf(stderr, "usage: split rom_folder [type]\n eg pinmame32_23/roms/game_name/ (note trailing slash)\ntype: sys6 or sys7");
+		return 1;
+	}
+	char* folder = argv[1];
 	FILE *in  = fopen("rom.764", "rb");
 	if(!in) {
 		fprintf(stderr, "could not open rom.764, errno %i", (errno));
 		return 3;
 	}
-	FILE *a = fopen("pinmame32_23/roms/httip_l1/gamerom.716", "wb");
-	FILE *aa = fopen("gamerom.716", "wb");
-	FILE *b = fopen("pinmame32_23/roms/httip_l1/white1.716", "wb");
-	FILE *bb = fopen("white1.716", "wb");
-	FILE *c = fopen("pinmame32_23/roms/httip_l1/white2.716", "wb");
-	FILE *cc = fopen("white2.716", "wb");
-	fseek(in, 0x0000, SEEK_SET);
-	write(in, a);
-	fseek(in, 0x0000, SEEK_SET);
-	write(in, aa);
+	char path[100];
+	FILE *a, *b, *c, *aa, *bb, *cc;
+	if (argc >= 3 && !strcmp(argv[2], "sys7")) {
+		sprintf(path, "%sic14.716", folder);
+		a = fopen(path, "wb");
+		sprintf(path, "%sic20.716", folder);
+		b = fopen(path, "wb");
+		sprintf(path, "%sic17.532", folder);
+		c = fopen(path, "wb");
+		aa = fopen("ic14.716", "wb");
+		bb = fopen("ic20.716", "wb");
+		cc = fopen("ic17.532", "wb");
+		fseek(in, 0x0000, SEEK_SET);
+		write(in, a, 2048);
+		fseek(in, 0x0000, SEEK_SET);
+		write(in, aa, 2048);
+		
+		fseek(in, 0x800, SEEK_SET);
+		write(in, b, 2048);
+		fseek(in, 0x800, SEEK_SET);
+		write(in, bb, 2048);
+		
+		fseek(in, 0x1000, SEEK_SET);
+		write(in, c, 2048*2);
+		fseek(in, 0x1000, SEEK_SET);
+		write(in, cc, 2048*2);
+	} else {
+		sprintf(path, "%sgamerom.716", folder);
+		a = fopen(path, "wb");
+		sprintf(path, "%swhite1.716", folder);
+		b = fopen(path, "wb");
+		sprintf(path, "%swhite2.716", folder);
+		c = fopen(path, "wb");
+		aa = fopen("gamerom.716", "wb");
+		bb = fopen("white1.716", "wb");
+		cc = fopen("white2.716", "wb");
+
+		fseek(in, 0x0000, SEEK_SET);
+		write(in, a, 2048);
+		fseek(in, 0x0000, SEEK_SET);
+		write(in, aa, 2048);
+		
+		fseek(in, 0x1000, SEEK_SET);
+		write(in, b, 2048);
+		fseek(in, 0x1000, SEEK_SET);
+		write(in, bb, 2048);
+		
+		fseek(in, 0x1800, SEEK_SET);
+		write(in, c, 2048);
+		fseek(in, 0x1800, SEEK_SET);
+		write(in, cc, 2048);
+	}
 	
-	fseek(in, 0x1000, SEEK_SET);
-	write(in, b);
-	fseek(in, 0x1000, SEEK_SET);
-	write(in, bb);
-	
-	fseek(in, 0x1800, SEEK_SET);
-	write(in, c);
-	fseek(in, 0x1800, SEEK_SET);
-	write(in, cc);
 	
 	fseek(in, 0x0000, SEEK_END);
 	if(ftell(in)!=8192) {
 		fprintf(stderr, "file overflow! %i", ftell(in));
 		return 1;
-	}
-	fseek(in, 0x0800, SEEK_SET);
-	while(ftell(in)<0x1000) {
-		if(fgetc(in) != 0) {
-			fprintf(stderr, "overflow at %x!", ftell(in) + 0x6000);
-			return 2;
-		}
 	}
 	fclose(a);
 	fclose(b);
